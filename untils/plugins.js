93a13d49410efe0527736805d73a6e1d162d8e9c
labi <1508138963@qq.com>
2020-12-09 22:58:06 +0800

    1209

--- untils/plugins.js
+++ untils/plugins.js
@@ -1,8 +1,8 @@
 import vue from 'vue';
 import vue-lazyload from 'vue-lazyload';
-vue.use(vue-lazyload,{
-	  preLoad: 1,
-	  error: require('@/static/img/home/default.jap'),
-	  loading: require('@/static/img/home/default.jap'),
-	  attempt: 2,
-});
+// vue.use(vue-lazyload,{
+// 	  preLoad: 1,
+// 	  error: require('@/static/img/home/default.jap'),
+// 	  loading: require('@/static/img/home/default.jap'),
+// 	  attempt: 2,
+// });
